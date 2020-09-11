a9562d68a591e80ce2199fc6cddb30aaccf64b5d
Quintin Kruger <qkruger@jhb.dvt.co.za>
2020-09-11 13:19:01 +0200

    Stale code removed - everything that was created per the openapi contract

--- src/main/java/za/co/pp/data/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package za.co.pp.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import za.co.pp.data.entity.ProductEntity;
-
-@Repository
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
