3821b8a0071dd6e3e8d76c1ff6392730cc59829b
Quintin Kruger <qkruger@jhb.dvt.co.za>
2020-09-21 20:17:57 +0200

    Removed commented out dependency from pom and yml property file commented out properties

--- src/main/java/za/co/pp/PrettyParadiseClientApplication.java
+++ src/main/java/za/co/pp/PrettyParadiseClientApplication.java
@@ -1,18 +1,11 @@
 package za.co.pp;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-//import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 @SpringBootApplication
 @EnableEurekaClient
-//@EnableJdbcHttpSession
 public class PrettyParadiseClientApplication {
 
     public static void main(String... args) {
